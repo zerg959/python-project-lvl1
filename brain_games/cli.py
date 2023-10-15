32e51596d8a6c0062fc052a3f6063ea3b65c3b4b
zerg959 <zerg959@yandex.ru>
2023-10-15 21:18:35 +0300

    add cli.py

---
+++ brain_games/cli.py
@@ -0,0 +1,12 @@
+#!/usr/bi/env python
+
+import prompt
+
+def welcome_user():
+    name = prompt.string('May I have your name? ')
+    print(f"Hello, {name}!")
+
+
+if __name__ == '__main__':
+    welcome_user()
+
